ab0d8e459160452894b66eb3ccfe416e7212f083
Jae min Baek <jbaek7023@gmail.com>
2017-02-03 20:19:37 +0900

    Finished Product app

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import Product, Variation, ProductImage, Category, ProductFeatured
 
+class VariationInline(admin.TabularInline):
+    model = Variation
+    extra = 0
+
+class ProductInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','price','description', 'active']
+    inlines = [
+        VariationInline,
+        ProductInline,
+    ]
+    class Meta:
+        model = Product
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation)
 admin.site.register(ProductImage)
 admin.site.register(Category)
 admin.site.register(ProductFeatured)
+
